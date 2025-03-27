b171e48f4b9c9c8634e2cc39ce38d81c0fe731a6
phhuongthao <npthao808@gmail.com>
2025-03-27 14:14:18 +0700

    admin

--- src/admin/components/ui/card.js
+++
@@ -1,8 +0,0 @@
-export const Card = ({ children }) => (
-    <div className="border p-4 rounded-md shadow bg-white">{children}</div>
-  );
-  
-  export const CardContent = ({ children }) => (
-    <div className="p-4">{children}</div>
-  );
-  
